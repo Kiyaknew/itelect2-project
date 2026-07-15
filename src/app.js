7609b551c5fb7ddbd64748f2ba801f1672e23ed5
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-07-15 14:48:03 +0800

    GT3: Add ES6+ utility functions

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 //app.js - Main Application Entry Point
+
+//importing functions from utils.js
+import { formatDate } from "./utils.js";
+import { validateTask } from "./utils.js";
+import { mergeTaskUpdate } from "./utils.js";
+
+//testing out functions
 console.log('Server starting. . .');
+console.log(formatDate(new Date("2026-07-22")));
+console.log(validateTask());
+console.log(validateTask({ title: "Bring index card", dueDate: "2026-07-17"}));
+console.log(mergeTaskUpdate({title: "Bring index card"}, {title: "Bring water bottle"} ));
+

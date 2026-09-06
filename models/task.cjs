e4c6e35a44a4131d067b7f615300e0239c1c4357
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-09-07 05:11:05 +0800

    GT8: Replace in-memory data with PostgreSQL

--- models/task.cjs
+++ models/task.cjs
@@ -4,17 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+       Task.belongsTo(models.User, { foreignKey: 'userId' });
     }
   }
   Task.init({
-    title: DataTypes.STRING,
+    title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: { notEmpty: { msg: 'title is required for tasks' } }
+    },
     dueDate: DataTypes.DATE,
     completed: DataTypes.BOOLEAN,
     userId: DataTypes.INTEGER
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { mockTasks, validateTask, mergeTaskUpdate, createTask, TaskValidationError } from "../src/utils.js";
+import db from "../models/index.cjs";
 
+const { Task, User } = db;
 const router = express.Router();
 
 let userList = [];
@@ -9,63 +10,45 @@ export function setUserList(users) {
     userList = users;
 }
 
-router.get("/tasks", (req, res) => {
-    res.json(mockTasks);
+router.get("/tasks", async (req, res) => {
+  const tasks = await Task.findAll({ include: User, order: [["id", "ASC"]] });
+  res.json(tasks);
 });
 
-router.get("/tasks/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const task = mockTasks.find((t) => t.id === id);
-
-    if (!task) {
-        res.status(404).json({ error: `Task with id ${req.params.id} not found` });
-        return;
-    }
-
-    res.json(task);
+router.get("/tasks/:id", async (req, res) => {
+  const task = await Task.findByPk(req.params.id, { include: User });
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+  res.json(task);
 });
 
-router.post("/tasks", (req, res, next) => {
-    try {
-        const task = createTask(req.body);
-        mockTasks.push(task);
-        res.status(201).json(task);
-    } catch (err) {
-        if (err instanceof TaskValidationError) {
-            err.status = 400;
-        }
-        next(err);
-    }
+router.get("/users", async (req, res) => {
+  const users = await User.findAll({ include: Task, order: [["id", "ASC"]] });
+  res.json(users);
 });
 
-//gt6!!
-
-router.put("/tasks/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const index = mockTasks.findIndex((t) => t.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({ error: `Task with id ${req.params.id} not found` });
-    }
-
-    mockTasks[index] = mergeTaskUpdate(mockTasks[index], req.body);
-    res.status(200).json(mockTasks[index]);
+router.post("/tasks", async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
 });
 
-router.delete("/tasks/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const index = mockTasks.findIndex((t) => t.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({ error: `Task with id ${req.params.id} not found` });
-    }
-
-    const [removed] = mockTasks.splice(index, 1);
-    res.status(200).json({ message: "Deleted", task: removed });
+router.put("/tasks/:id", async (req, res) => {
+  const task = await Task.findByPk(req.params.id);
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+  await task.update(req.body);
+  res.json(task);
 });
 
-router.get("/users", (req, res) => {
-    res.json(userList);
+router.delete("/tasks/:id", async (req, res) => {
+  const task = await Task.findByPk(req.params.id);
+  if (!task) {
+    return res.status(404).json({ error: "Task not found!" });
+  }
+  await task.destroy();
+  res.json({ message: "Deleted :)", task });
 });
 
 
